f94c3cbdc84535ba152d3c56150e5683999918c4
Victor Okeke <okekevicktur@gmail.com>
2022-08-29 23:37:12 +0100

    new modules

--- app.js
+++ app.js
@@ -1,13 +1,39 @@
 const os = require('os');
-
-var tm=os.totalmem();
-var fm=os.freemem();
+//Operating system
+// var tm=os.totalmem();
+// var fm=os.freemem();
  
-console.log('Total Memory '+ tm);
-console.log('Free Memory '+ fm);
-
-const fs = require('fs');
-const files= fs.readdirSync('./');
-console.log(files);
-
+// console.log('Total Memory '+ tm);
+// console.log('Free Memory '+ fm);
+// //File System
+// const fs = require('fs');
+// const files= fs.readdirSync('./');
+// console.log(files);
 // /Events
+const EventEmitter = require('events');
+const emitter = new EventEmitter();
+
+// emitter.addListener();
+//register a listener
+emitter.addListener('messageLogged', function(){
+    console.log('Listener called');
+});
+
+//Raise an event
+emitter.emit('messageLogged'); 
+//signals that an event has happened
+
+//Http Module
+const http= require('http');
+// const { Socket } = require('dgram');
+const server    = http.createServer((req, res)=>{
+    if (req.url ==='/'){
+        res.write('helloWorld');
+        res.end();
+    }
+});
+// server.on('connection', (Socket) => {
+//     console.log('New Connection....');
+// }); 
+server.listen(3000);
+console.log('Listening on port 3000...');
